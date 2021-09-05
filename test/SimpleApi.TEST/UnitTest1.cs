72540d965e427ea68095ac41cfb85305c65336fd
Tahir Bencheikh <tahir.bencheikh@ia.ca>
2021-09-05 18:56:24 -0400

    Initial commit

---
+++ test/SimpleApi.TEST/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Moq;
+using Xunit;
+using SimpleApi.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleApi.TEST
+{
+    public class UnitTest1
+    {
+       
+
+        [Fact]
+        public void TestName()
+        {
+            var wc= new WeatherForecastController();
+            //Given
+            var returnValue = wc.Get();
+            //When
+            
+            //Then
+            Assert.Null(returnValue);
+        }
+    }
+}
