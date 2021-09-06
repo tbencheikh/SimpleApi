abaf41b28a817f7ce825f8df0ed818f7b67ad82b
Tahir Bencheikh <tahir.bencheikh@ia.ca>
2021-09-05 21:37:11 -0400

    removed https redirection

--- src/SimpleApi/Startup.cs
+++ src/SimpleApi/Startup.cs
@@ -46,7 +46,7 @@ namespace SimpleApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleApi v1"));
             }
 
-            app.UseHttpsRedirection();
+          
 
             app.UseRouting();
 
