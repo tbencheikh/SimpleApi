38062fa6b08e78ad8c672fca78966ac1d218f0c6
Tahir Bencheikh <tahir.bencheikh@ia.ca>
2021-09-06 02:26:34 -0400

    fix test1

--- src/SimpleApi/Controllers/WeatherForecastController.cs
+++ src/SimpleApi/Controllers/WeatherForecastController.cs
@@ -16,7 +16,7 @@ namespace SimpleApi.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        //private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController()
         {
